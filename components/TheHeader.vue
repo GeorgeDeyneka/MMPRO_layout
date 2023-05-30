e15a0d6875e6f1e5ac8b665318631792045a390c
GeorgeDeyneka <zhoradeynecka@gmail.com>
2023-05-30 21:59:00 +0300

    Basic styling and fonts, create header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <img class="header__logo" src="@/assets/images/logo.png" alt="MMPRO logo" />
+
+    <BurgerMenu />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 0 20px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-between;
+}
+.header {
+  &__logo {
+    padding: 6px 2px;
+  }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Hello World</h1>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.container {
+  padding: 0 20px;
+}
+</style>
