02b629083fa16abe62f53a43ad67802cd95e26b4
GeorgeDeyneka <zhoradeynecka@gmail.com>
2023-05-31 16:17:42 +0300

    Add base text-content on all pages (without styles)

---
+++ components/TheForm.vue
@@ -0,0 +1,8 @@
+<template>
+  <form>
+    <input type="text" />
+    <input type="text" />
+    <input type="text" />
+    <TheButton text="Send" btn-type="submit" />
+  </form>
+</template>
--- pages/become.vue
+++ pages/become.vue
@@ -1,7 +1,75 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Become</h1>
+  <main class="become">
+    <h2 class="become__title">
+      Become
+      <span class="green-span"> 1 of 100 </span>
+      revenue sharing NFT holders and share up to
+      <span class="green-span"> 10% </span>
+      from the NFT Store revenue
+    </h2>
+
+    <div class="become__block value">
+      <h4 class="green-subtitle">The value of NFT</h4>
+      <p>
+        MMPro Labs - is an exclusive investors club with only 100 members. They
+        share 10% of the NFT-Store revenue and enjoy other fantastic benefits.
+      </p>
+      <p>
+        The price of 1 NFT is 200K $MMPro tokens which is now an equivalent of
+        $50K.
+      </p>
+    </div>
+
+    <div class="become__block get-it">
+      <h4 class="green-subtitle">How you can get it?</h4>
+      <p>
+        Purchase any of the pre-IPO secondaries worth of
+        <span class="green-span"> $100K </span>
+        on Trust Ventures website. Our teammates on the MMPro booth will help
+        you with that.
+      </p>
+    </div>
+
+    <div class="become__block rewards">
+      <h4 class="green-subtitle">Rewards</h4>
+      <p>
+        We will draw
+        <span class="green-span"> 1 MMPRo Labs NFT </span>
+        on our booth between people, who has met the requirements and purchased
+        secondaries
+        <span class="green-span"> worth of $100K. </span>
+      </p>
+      <p>Winners will be determined by random.org</p>
+    </div>
+
+    <div class="become__block draw">
+      <h4 class="green-subtitle">Draw Date</h4>
+      <p>
+        The draw will happen on the
+        <span class="green-span"> 30th March </span>
+        on MMPro booth preliminary
+        <span class="green-span"> 4 PM HG time. </span>
+      </p>
+
+      <p>
+        If you don't have a chance to come we will record the draw process on
+        video and share it with you in email.
+      </p>
+
+      <p>
+        Please keep an eye on your email as we will send all the information
+        there.
+      </p>
+    </div>
+
+    <TheForm />
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.become {
+  padding: 20px;
+}
+</style>
